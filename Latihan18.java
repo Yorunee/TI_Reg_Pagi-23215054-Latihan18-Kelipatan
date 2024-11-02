02ab341ad67637de7946e1ba096fef54972407e9
Yorunee <23215054@mhs.stmik-tegal.ac.id>
2024-11-02 20:34:48 +0700

    Add files via upload

---
+++ Latihan18.java
@@ -0,0 +1,17 @@
+
+package Pertemuan5;
+
+
+public class Latihan18 {
+     public static void main(String[] args) {
+        // Inisialisasi variabel awal
+        double angka = 3.5;
+
+        // Loop untuk mencetak angka kelipatan 3.5 hingga 35
+        while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5; // Tambah 3.5 setiap iterasi
+        }
+    }
+}
+    
